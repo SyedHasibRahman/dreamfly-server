b66550ec6e9d6d7b704d0b5297e72d6efe341a24
SyedHasibRahman <syedhasib01@gmail.com>
2022-03-16 21:10:19 +0600

    Change

--- index.js
+++ index.js
@@ -23,62 +23,62 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 //  console.log(uri);
 
 async function run() {
-    try {
-        await client.connect();
+  try {
+    await client.connect();
 
-        const database = client.db('DreamFly');
-        const blogsCollection = database.collection('blogs');
+    const database = client.db('DreamFly');
+    const blogsCollection = database.collection('blogs');
 
-        // GET Blogs API
-        app.get('/blogs', async (req, res) => {
-            const cursor = blogsCollection.find({});
-            const blogs = await cursor.toArray();
-            res.send(blogs);
-        });
+    // GET Blogs API
+    app.get('/blogs', async (req, res) => {
+      const cursor = blogsCollection.find({});
+      const blogs = await cursor.toArray();
+      res.send(blogs);
+    });
 
-        // //GET blogs API
-        // app.get('/blogs', async (req, res) => {
-        //     const cursor = blogsCollection.find({});
-        //     const page = req.query.page;
-        //     const size = parseInt(req.query.size);
-        //     let blogs;
-        //     const count = await cursor.count();
+    // //GET blogs API
+    // app.get('/blogs', async (req, res) => {
+    //     const cursor = blogsCollection.find({});
+    //     const page = req.query.page;
+    //     const size = parseInt(req.query.size);
+    //     let blogs;
+    //     const count = await cursor.count();
 
-        //     if (page) {
-        //         blogs = await cursor.skip(page * size).limit(size).toArray();
-        //     }
-        //     else {
-        //         blogs = await cursor.toArray();
-        //     }
+    //     if (page) {
+    //         blogs = await cursor.skip(page * size).limit(size).toArray();
+    //     }
+    //     else {
+    //         blogs = await cursor.toArray();
+    //     }
 
-        //     res.send({
-        //         count,
-        //         blogs
-        //     });
-        // });
+    //     res.send({
+    //         count,
+    //         blogs
+    //     });
+    // });
 
-        //GET Single blog
-        app.get('/blogs/:id', async(req, res) => {
-            const id = req.params.id;
-            const query = {_id: ObjectId(id)};
-            const blog = await blogsCollection.findOne(query);
-            res.json(blog);
-        });
+    //GET Single blog
+    app.get('/blogs/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const blog = await blogsCollection.findOne(query);
+      res.json(blog);
+    });
 
 
-        console.log('DB Connected');
-    } finally {
-        // await client.close();
-    }
+    console.log('DB Connected');
+  } finally {
+    // await client.close();
+  }
 }
 
 run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Hello DreamFly!')
+  res.send('Hello DreamFly!')
 })
 
 app.listen(port, () => {
-    console.log(`listening on port ${port}`)
+  console.log(`listening on port ${port}`)
 })
